a5225b11971154f45cf7b1ea5ca4e8c8b38ab4f0
kkavon <61874951+kkavon@users.noreply.github.com>
2023-04-05 11:57:38 +0100

    Implementing ShoppingCart Features(session changes) and Fixing Thymeleaf Template Errors

--- OrderItemRepository.java
+++ OrderItemRepository.java
@@ -1,8 +1,11 @@
 package net.codejava;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+	List<OrderItem> findByOrder(Orders order);
 }
